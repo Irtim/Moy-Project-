a20b49d16f8e7da27717b52a478fed2f5d48af10
movavi_school <mdiscool1@mail.ru>
2022-10-06 11:45:05 +0700

    Прошёл 3 урок,я всё понимал кроме последней задачи(

---
+++ lesson_3/main.py
@@ -0,0 +1,50 @@
+# # # большой string
+# x = 'Артём\nАртём\nАртём'
+# x1 ='''Строка1
+# Строка2
+# Строка3'''
+# print (x1)
+# #name_1 = 'Артём'
+# name_2 = 'Никита'
+# name_3 = 'Вова'
+# bratva = [name_1, name_2, name_3]
+# print (bratva)
+# print (bratva[0])
+# print (bratva[-3])
+# #alphabet = 'АБВГДЕЁЖЗИКЛМН'
+# print(alphabet[0])
+# print(alphabet[-1])
+# print(alphabet[0] + alphabet[1] + alphabet[2])
+# print(alphabet[6] + alphabet[7] + alphabet[9] + alphabet[10])
+# print(alphabet[-4:])#вывести 4 последних символов
+# print(alphabet[:3])#вывести 3 первых символа
+# print(alphabet[:6:2])#вывести через одного среди первых шести символов
+# print(alphabet[::-1]) #вывести в обратном порядке
+# print(alphabet[::-2]) #вывести в обратном порядке через одного
+#x  = input('Введи слово:')
+#temp = x[-1]#сохранили последний символ
+#print(x.index(temp) + 1)#индекс последнего символа
+#print(len(x))
+# print('Первая буква:', x[0])
+# print('Последняя буква:', x[-1])
+#path = "C:/pythongfhgfh/python.exe"
+#решение но не крутое
+# #print('Name File: ', path[-10:])
+# print('Расширение: ', path[-3:])
+# print('Имя каталога: ', path[3:10])
+# print('Полный путь к каталогу: ', path[:10])
+# #path = "C:/pythongfhgfh/python.exe"
+# temp = path.split('/')
+# print(temp)
+# print('Name File: ', temp [-1])
+# print('Расширение: ', temp [-1][-3:])
+# print('Имя каталога: ', temp[-2])
+# print('Полный путь к каталогу: ', temp [0] + '/' + temp[1] )
+# #chapter_1 = input('Глава 1: ')
+# page_1 = input('Страница : ')
+#
+# chapter_2 = input('Глава 2: ')
+# page_2 = input('Страница : ')
+#
+# print(chapter_1.rjust(15), page_1.ljust(15))
+# print(chapter_2.rjust(15), page_2.ljust(15))
