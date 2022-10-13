b868777757b9d705645447a7b44e73a7ed40b5ec
movavi_school <mdiscool1@mail.ru>
2022-10-13 11:34:10 +0700

    13 октября, хочу спать,урок был интересным

---
+++ lesson_4/main.py
@@ -0,0 +1,39 @@
+#world = input('Веди Фразочку: ')
+#print('аб:', world.count ('аб'))
+# familiya = input("Фамилия:").capitalize()
+# imya = input("Имя:").capitalize()
+# otchestvo = input("Отчество:").capitalize()
+# # capitalize - первая буква заглавная, остальные маленькие
+#
+# # два варианта одинакового вывода
+# print(familiya, imya[0] + ".", otchestvo[0] + ".")
+# print(f"{familiya} {imya[0]}. {otchestvo[0]}.")
+# #pharese = input('Введи фразу, разделенную пробелами:').strip()
+# lst = pharese.split(' ')
+# print(lst)
+# lst.remove("Artem")  # удаление по значению
+# lst.pop(0)  # удаление по индексу
+# print(lst)
+# #phrase = input("Введи фразу:")
+# find = input("Что меняем:")
+# replace = input("На что меняем:")
+# print (phrase.replace(find, replace))
+# x = input()
+#print(x.replace("йо", "ё"))
+# alphabet = {
+# "а": "a",
+# "б": "b",
+# "в": "v",
+# "г": "g",
+# "д": "d",
+# "е": "e",
+# "ё": "yo",
+# "ж": "zh",}
+# phrase = input('Введи фразу:')
+# translate = ''
+# for bukva in phrase:
+#     translate = translate + alphabet[bukva]
+# print(translate)
+
+
+
